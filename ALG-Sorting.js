d45ca57ffb5b14caff533ecf319f224b1fd10f80
Tala261012 <tala261012@gmail.com>
2024-10-26 22:18:39 +0300

    Sorting 3 : Quick Sort

--- ALG-Sorting.js
+++ ALG-Sorting.js
@@ -33,14 +33,43 @@ function selectionSort(arr) {
 console.log(selectionSort([1, 4, 12, 99, 0, 100]));
 // =============================================================================
 
-//? Быстрая сортировка, рекурсия, quick sort
+//? Рекурсия, сумма всех элементов массива =====================================
+
+const myArr = [1, 5, 12, 99, 0, 100]; //216
+
+function summArr(arr) {
+  // if (arr.length === 0) return 0; // базовый случай, не обязательный
+  if (arr.length === 1) return arr[0]; // базовый случай
+  else {
+    const temp = arr.splice(0, 1);
+    const sum = temp[0] + summArr(arr);
+    return sum;
+  }
+}
+
+console.log(summArr(myArr));
+// =============================================================================
+
+//? Быстрая сортировка, рекурсия, quick sort ===================================
 // значительные затраты памяти на стек вызовов
 // когда в рекурсивной функции задействован массив, то базовый случай - это
 // пустой массив или массив из одного элемента
 
+//выбираем опорный элемент и находим элементы больше и меньше него
+
+const arrForSorting = [17, 23, 0, 16, 98, 55, 4, 77, -1];
+
+// сортировка по возрастанию
 function quickSort(arr) {
-  return;
+  if (arr.length < 2) return arr;
+  else {
+    const base = arr[0]; //опорный элемент
+    const less = arr.filter((el) => el < base); // меньшие
+    const greater = arr.filter((el) => el > base); // большие
+
+    return quickSort(less).concat(arr[0], quickSort(greater)); // быстрая сортировка
+  }
 }
 
-console.log(quickSort([1, 4, 12, 99, 0, 100]));
+console.log(quickSort(arrForSorting));
 // =============================================================================
