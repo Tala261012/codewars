f2cfae3dfa3d439b477936568278b63d18f84c09
Tala261012 <tala261012@gmail.com>
2024-12-28 18:06:27 +0200

    Learning JS - weakSet & weakMap added

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-// Анаграммы – это слова, у которых те же буквы в том же количестве,
-//но они располагаются в другом порядке.
-// Например:
-// nap - pan
-// ear - are - era
-// cheaters - hectares - teachers
-// Напишите функцию aclean(arr), которая возвращает массив слов, очищенный от анаграмм.
-function aclean(arr) {
-  let map = new Map();
+//* WeakMap =========== не итерабельный ===============================================
+/*
+Первое его отличие от Map в том, что ключи в WeakMap ДОЛЖНЫ быть _объектами_, 
+а не примитивными значениями
+*/
+let weakMap = new WeakMap();
+let obj = { name: "John" };
+weakMap.set(obj, "ok");
+console.log(weakMap.get(obj)); //ok
+/*
+Теперь, если мы используем объект в качестве ключа 
+и если больше НЕТ ССЫЛОК на этот объект, то он будет УДАЛЕН  из памяти 
+(и из объекта WeakMap!) автоматически.
+*/
+obj = null;
+console.log(weakMap.get(obj)); //undefined
+/*
+НЕ поддерживает перебор и методы keys(), values(), entries(), 
+так что нет способа взять все ключи или значения из неё.
 
-  for (let word of arr) {
-    let sorted = word.toLowerCase().split("").sort().join();
-    map.set(sorted, word);
-  }
+присутствуют ТОЛЬКО следующие методы:
 
-  return Array.from(map.values());
-}
-let arrAnagr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
-console.log(aclean(arrAnagr));
+? weakMap.get(key)
+? weakMap.set(key, value)
+? weakMap.delete(key)
+? weakMap.has(key)
+
+Нет возможности получить size, так как это WeakMap.
+В основном, WeakMap используется в качестве дополнительного хранилища данных, для кеширования.
+*/
+
+//* WeakSet =========== не итерабельный ===============================================
+/*
+Аналогично Set, но добавлять можно только _объекты_ (не примитивные значения)
+Объект присутствует в WeakSet только до тех пор, пока доступен где-то ещё.
+
+Как и Set, она поддерживает 
+? weakset.add(obj) 
+? weakset.has(obj) 
+? weakset.delete(obj)
+но НЕ size, keys() и не является перебираемой
+*/
