df274cd62b287e3944b66981c4ecf11380b37d0d
Tala261012 <tala261012@gmail.com>
2025-02-06 18:57:00 +0200

    Learning JS - Object.create(null)

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+let dictionary = Object.create(null);
 
+Object.defineProperty(dictionary, "toString", {
+  value() {
+    return Object.keys(this).join(",");
+  },
+});
+
+dictionary.apple = "Apple";
+dictionary.__proto__ = "test"; // здесь __proto__ -- это обычный ключ
+
+// только apple и __proto__ выведены в цикле
+for (let key in dictionary) {
+  console.log(key); // "apple", затем "__proto__"
+}
+
+// ваш метод toString в действии
+console.log(dictionary.toString()); // "apple,__proto__"
--- tasks-javascript-ru.js
+++ tasks-javascript-ru.js
@@ -622,3 +622,27 @@ function f(a, b) {
 
 f.defer(1000)(1, 2); // 3
 // ===============================
+
+/* Имеется объект dictionary, созданный с помощью Object.create(null) 
+для хранения любых пар ключ/значение. Добавьте ему метод dictionary.toString(), 
+который должен возвращать список ключей, разделённых запятой. 
+Ваш toString не должен выводиться при итерации объекта с помощью цикла for..in.*/
+let dictionary = Object.create(null);
+
+Object.defineProperty(dictionary, "toString", {
+  value() {
+    return Object.keys(this).join(",");
+  },
+});
+
+dictionary.apple = "Apple";
+dictionary.__proto__ = "test"; // здесь __proto__ -- это обычный ключ
+
+// только apple и __proto__ выведены в цикле
+for (let key in dictionary) {
+  console.log(key); // "apple", затем "__proto__"
+}
+
+// ваш метод toString в действии
+console.log(dictionary.toString()); // "apple,__proto__"
+// ===============================
