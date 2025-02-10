1c30274e56b7df7838d5fbc797b5a12499ee17e1
Tala261012 <tala261012@gmail.com>
2025-02-10 18:29:39 +0200

    Learning JS - static methods & properties

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-class Animal {
-  name = "animal";
+class Article {
+  constructor(title, date) {
+    this.title = title;
+    this.date = date;
+  }
 
-  constructor() {
-    console.log(this.name); // (*)
+  static publisher = "Илья Кантор";
+
+  static compare(articleA, articleB) {
+    return articleA.date - articleB.date;
+  }
+
+  static createTodays() {
+    // помним, что this = Article
+    return new this("Сегодняшний дайджест", new Date());
   }
 }
 
-class Rabbit extends Animal {
-  name = "rabbit";
-}
+// использование
+let articles = [
+  new Article("HTML", new Date(2019, 1, 1)),
+  new Article("CSS", new Date(2019, 0, 1)),
+  new Article("JavaScript", new Date(2019, 11, 1)),
+];
 
-let animal = new Animal(); // animal
-let rabbit = new Rabbit(); // animal
-console.log(rabbit.name);
+articles.sort(Article.compare);
+console.log(articles[0].title); // CSS
+console.log(Object.getOwnPropertyDescriptors(articles[0]));
+
+let today = Article.createTodays();
+console.log(today.publisher);
