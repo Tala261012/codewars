d2cfda69ad445c9a45e7289a7317aa2c4e8351ae
Tala261012 <tala261012@gmail.com>
2025-02-21 17:04:10 +0200

    Learning JS - Promise methods

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-/* Встроенная функция setTimeout использует колбэк-функции. 
-Создайте альтернативу, использующую промисы.
-Функция delay(ms) должна возвращать промис, который перейдёт в состояние 
-«выполнен» через ms миллисекунд, так чтобы мы могли добавить к нему .then:
-*/
-function delay(ms) {
-  return new Promise((resolve) => setTimeout(() => resolve(), ms));
-}
-delay(3000).then(() => console.log("выполнилось через 3 секунды"));
+let urls = [
+  "https://api.github.com/users/iliakan",
+  "https://api.githubи.com/users/remy",
+  "https://api.github.com/users/jeresig",
+];
+
+// Преобразуем каждый URL в промис, возвращённый fetch
+let requests = urls.map((url) => fetch(url));
+
+console.log(requests);
+Promise.allSettled(requests)
+  .then((responses) =>
+    responses.forEach((response, num) => {
+      if (response.status == "fulfilled") {
+        console.log(`${num}: ${response.value.status}`);
+      }
+      if (response.status == "rejected") {
+        console.log(`${num}: ${response.reason}`);
+      }
+    })
+  )
+  .catch(console.log);
