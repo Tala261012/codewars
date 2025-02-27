65bbd3f37fe195aede56afcdc835acad171c10ab
Tala261012 <tala261012@gmail.com>
2025-02-27 20:56:25 +0200

    Learning JS - yield task

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-function* gen() {
-  let ask1 = yield "2 + 2 = ?";
-
-  console.log(ask1); // 4
-
-  let ask2 = yield "3 * 3 = ?";
-
-  console.log(ask2); // 9
+// создать функцию-генератор pseudoRandom(seed),
+// которая получает seed и создаёт генератор с указанной формулой.
+// next = previous * 16807 % 2147483647
+function* pseudoRandom(seed) {
+  let value = seed;
+  while (true) {
+    value = (value * 16807) % 2147483647;
+    yield value;
+  }
 }
 
-let generator = gen();
+let generator = pseudoRandom(1);
 
-console.log(generator.next().value); // "2 + 2 = ?"
-
-console.log(generator.next(4).value); // "3 * 3 = ?"
-
-console.log(generator.next(9).done); // true
+console.log(generator.next().value); // 16807
+console.log(generator.next().value); // 282475249
+console.log(generator.next().value); // 1622650073
--- tasks-javascript-ru.js
+++ tasks-javascript-ru.js
@@ -729,3 +729,22 @@ function f() {
 }
 f();
 // ===============================
+
+//? yield =======================================================================
+// создать функцию-генератор pseudoRandom(seed),
+// которая получает seed и создаёт генератор с указанной формулой.
+// next = previous * 16807 % 2147483647
+function* pseudoRandom(seed) {
+  let value = seed;
+  while (true) {
+    value = (value * 16807) % 2147483647;
+    yield value;
+  }
+}
+
+let generator = pseudoRandom(1);
+
+console.log(generator.next().value); // 16807
+console.log(generator.next().value); // 282475249
+console.log(generator.next().value); // 1622650073
+// ===============================
